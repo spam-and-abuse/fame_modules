ff121151c64393452b0266e69cc1d5da855c0ee5
coldy <coldy@telchar>
2018-01-12 18:00:16 +0100

    typo

--- processing/virustotal_pub/virustotal_pub.py
+++ processing/virustotal_pub/virustotal_pub.py
@@ -13,42 +13,41 @@ class VirusTotalPub(ProcessingModule):
             'description': 'Virustotal API key'
         },
     ]
-   
+
     def each_with_type(self, target, obj_type):
         vt_api_url = 'http://www.virustotal.com/vtapi/v2/{method!s}/report'
-        params = {'apikey': self.VT_API_KEY,`
-                  'resource': None }
-        
+        params = {'apikey': self.VT_API_KEY,
+                  'resource': None}
+
         self.results = {
             'VT': u'',
             'Detections': u'',
             'Raw': u''
         }
 
-        if (obj_type == 'file'):
-            method = 'file'  
-            # calulate target hash 
-            h = hashlib.sha256() 
+        if obj_type == 'file':
+            method = 'file'
+            # calulate target hash
+            h = hashlib.sha256()
             with open(target, 'rb') as f:
                 h.update(f.read())
             params['resource'] = h.hexdigest()
-        elif (obj_type == 'url'):
+        elif obj_type == 'url':
             method = 'url'
             params['resource'] = target
-       
+
         r = requests.get(url=vt_api_url.format(method=method), params=params)
         if r.status_code == 200:
             self.log('debug', 'VirusTotal message: {}'.format(r.json()['verbose_msg']))
-           
+
             if r.json()['response_code'] == 1:
                 self.results['Detections'] = u'{}/{}'.format(r.json()['positives'], r.json()['total'])
                 self.results['VT'] = r.json()['permalink']
             self.results['Raw'] = r.json()
- 
+
         elif r.status_code == 204:
             self.log('warning', 'VirusTotal public API request rate limit exceeded. Try again later.')
         elif r.status_code == 403:
             self.log('error', 'You have tried to perform calls to VirusTotal\'s functions for which you do not have the required privileges.')
 
         return True
-
